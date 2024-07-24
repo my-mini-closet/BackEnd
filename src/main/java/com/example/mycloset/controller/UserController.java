b50c72a2e177096d6f69f7dbce575a3cdcd5e9d8
jhkim <juho1589@gmail.com>
2024-07-25 00:33:02 +0900

    PersonalColor api to mysql from flutter

--- src/main/java/com/example/mycloset/controller/UserController.java
+++ src/main/java/com/example/mycloset/controller/UserController.java
@@ -1,13 +1,24 @@
 package com.example.mycloset.controller;
 
+import com.example.mycloset.dto.UserDTO;
 import com.example.mycloset.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
-@RequestMapping("")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    @PostMapping("/updatePersonalColor")
+    public UserDTO updatePersonalColor(
+            @RequestParam String email,
+            @RequestParam String personalColor) throws IOException {
+        return userService.updatePersonalColor(email, personalColor);
+    }
 }
