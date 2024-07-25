8dee2479e0f54a31c0766da216c1914735f45d21
jhkim <juho1589@gmail.com>
2024-07-25 15:43:03 +0900

    BoardImageRepository create

--- src/main/java/com/example/mycloset/controller/BoardController.java
+++ src/main/java/com/example/mycloset/controller/BoardController.java
@@ -1,13 +1,29 @@
 package com.example.mycloset.controller;
 
+import com.example.mycloset.entity.Board;
 import com.example.mycloset.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/api/boards")
 @RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
+
+    @PostMapping("/create")
+    public Board createBoardWithImages(
+            @RequestParam("title") String title,
+            @RequestParam("text") String text,
+            @RequestParam("userId") Long userId,
+            @RequestParam("files") List<MultipartFile> files) throws IOException {
+        return boardService.saveBoardWithImages(title, text,userId,files);
+    }
 }
--- src/main/java/com/example/mycloset/service/BoardService.java
+++ src/main/java/com/example/mycloset/service/BoardService.java
@@ -1,13 +1,75 @@
 package com.example.mycloset.service;
 
+import com.example.mycloset.entity.Board;
+import com.example.mycloset.entity.BoardImage;
+import com.example.mycloset.entity.User;
 import com.example.mycloset.repository.BoardRepository;
 import com.example.mycloset.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
     private final BoardRepository boardRepository;
     private final UserRepository userRepository;
+
+    // application.yaml 파일에서 설정한 값을 가져온다
+    @Value("${image.upload.dir}")
+
+    private String uploadDir;
+
+    @Value("${image.access.url}")
+    private String accessUrl;
+
+
+    // @Transactional(readOnly = true)
+
+
+    public Board saveBoardWithImages(String title, String text, Long userId, List<MultipartFile> files) throws IOException {
+        // userId를 이용하여 user 테이블의 해당 사용자 정보를 가져온다
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        Board board = Board.builder()
+                .title(title)
+                .text(text)
+                .user(user)
+                .build();
+
+        // 실제 경로 가져오기
+        File staticImageDir = new ClassPathResource("static/images").getFile();
+        String realPath = staticImageDir.getAbsolutePath();
+
+        List<BoardImage> images = new ArrayList<>();
+        for (MultipartFile file : files) {
+            String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+            String filePath = realPath + File.separator + fileName;
+            String fileUrl = accessUrl + "/" + fileName;
+
+            File dest = new File(filePath);
+            file.transferTo(dest);
+
+            images.add(BoardImage.builder()
+                    .url(fileUrl)
+                    .path(filePath)
+                    .board(board)
+                    .build());
+        }
+        // Board와 Image의 관계 설정, board에 이미지 리스트를 저장
+        board.setImages(images);
+
+        return boardRepository.save(board);
+    }
+
 }
